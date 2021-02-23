7a12d37c50d0c8b4c3816e1bb76ecfd6a3379ae6
morecan <olympspartan@gmail.com>
2021-02-23 20:42:58 +0200

    change

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,7 @@ class Post < ApplicationRecord
   validate :some
   #TODO: add comment!
   #sdsds
-
-
   def some
-
-
 
     if self.title.length < 3
       errors.add :title, message: 'must be more than 2 letters'
