e5b8af8123d1fdabc89640630075175de1a82936
morecan <olympspartan@gmail.com>
2021-02-23 18:59:32 +0200

    change

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+
+  validate :some
+
+  def some
+    if self.title.length < 3
+      errors.add :title, message: 'must be more than 2 letters'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  scope :pages, controller: :pages do
+    get 'home'
+    get 'about'
+    get 'contacts'
+  end
   resources :posts
   root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
